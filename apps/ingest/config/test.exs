8e913f2c01746b745f84600ce8f3326b8d19eaba
Michael Klein <mbklein@gmail.com>
2019-05-03 12:22:19 -0500

    Add minio to circleci build

--- apps/ingest/config/test.exs
+++ apps/ingest/config/test.exs
@@ -6,6 +6,6 @@ config :ex_aws, :s3,
   access_key_id: "minio",
   secret_access_key: "minio123",
   host: "localhost",
-  port: 9002,
+  port: if(System.get_env("CI"), do: 9000, else: 9002),
   scheme: "http://",
   region: "us-east-1"
